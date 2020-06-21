a8eced7a6521bc5f41b69c4bde746055387a99f6
Rohit Kumawat <rohit.kumawat@primathon.in>
2020-06-21 23:05:22 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="title">
+        <span>Rohit Kumawat</span>
+        <br></br>
+        <span>Frontend Developer</span>
+        <br></br>
+        <span>catch me on twitter @geekrk</span>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
