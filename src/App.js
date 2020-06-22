88cea9c12b0da35727fab91739dbfbc7cf19a07c
Rohit Kumawat <rohit.kumawat@primathon.in>
2020-06-22 11:40:00 +0530

    homepage changed again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import Header from './components/header/header'
 
 function App() {
@@ -7,7 +7,11 @@ function App() {
     <div className="App">
       <Header></Header>
       <div className="intro">
-        <h1>Hi I am a Frontend Developer</h1>
+        <h1>Hi, I am a Frontend Developer</h1>
+        <h2>Javascript, Angular, React</h2>
+        <h2>
+          Catch me on twitter <span>@geekrk</span>
+        </h2>
       </div>
     </div>
   )
