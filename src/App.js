b3b08c7fe84628e686061d696d6d4bb62440958c
Rohit Kumawat <rkumawat125@gmail.com>
2020-07-05 20:33:03 +0530

    added mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -37,18 +37,22 @@ function App() {
       <div className="tech-stack-container">
         <div className="tech-stack">
           <span>My Favourite Tech Stack: </span>
-          <img src={typescript} alt="typescript" />
-          <img src={js} alt="js" />
-          <img src={react} alt="react" />
-          <img src={angular} alt="angular" style={{ width: '100px' }} />
+          <div className="tech-stack__logos">
+            <img src={typescript} alt="typescript" />
+            <img src={js} alt="js" />
+            <img src={react} alt="react" />
+            <img src={angular} alt="angular" style={{ width: '100px' }} />
+          </div>
         </div>
         <div className="tech-stack">
           <span>Currently working on: </span>
-          <img
-            src="https://img.icons8.com/fluent/96/000000/chrome.png"
-            alt="chrome"
-          />
-          Chrome Extensions
+          <div className="tech-stack__logos">
+            <img
+              src="https://img.icons8.com/fluent/96/000000/chrome.png"
+              alt="chrome"
+            />
+            Chrome Extensions
+          </div>
         </div>
       </div>
 
