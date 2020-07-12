d72bc4428beb24103362d61e0015cd3d33912802
Rohit Kumawat <rkumawat125@gmail.com>
2020-07-12 20:06:45 +0530

    tech component added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import background from './assets/startup.svg'
 import Blog from './components/blogs/blogs'
-import js from './assets/js-icon.svg'
-import react from './assets/react-icon.svg'
-import angular from './assets/angular-icon.svg'
-import typescript from './assets/typescript-icon.svg'
+import Tech from './components/tech-stack/tech'
 
 function App() {
   function clickLink() {
@@ -43,27 +40,7 @@ function App() {
         </section>
       </main>
 
-      <div className="tech-stack-container">
-        <div className="tech-stack">
-          <span>My Favourite Tech Stack: </span>
-          <div className="tech-stack__logos">
-            <img src={typescript} alt="typescript" />
-            <img src={js} alt="js" />
-            <img src={react} alt="react" />
-            <img src={angular} alt="angular" style={{ width: '100px' }} />
-          </div>
-        </div>
-        <div className="tech-stack">
-          <span>Currently working on: </span>
-          <div className="tech-stack__logos">
-            <img
-              src="https://img.icons8.com/fluent/96/000000/chrome.png"
-              alt="chrome"
-            />
-            Chrome Extensions
-          </div>
-        </div>
-      </div>
+      <Tech></Tech>
 
       <Blog></Blog>
 
