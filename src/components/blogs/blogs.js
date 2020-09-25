428c10676a7f57e297c9b44562d0385ee1071373
Rohit Kumawat <rkumawat125@gmail.com>
2020-09-25 14:58:23 +0530

    Update blogs.js

--- src/components/blogs/blogs.js
+++ src/components/blogs/blogs.js
@@ -1,35 +1,27 @@
 import React from 'react'
 import './blogs.scss'
-import array from '../../assets/array.png'
-import objects from '../../assets/objects.png'
-import js from '../../assets/js.png'
-import shouldComponentUpdate from '../../assets/shouldComponentUpdate.png'
 
 function Blog() {
   const mainObjArr = [
     {
-      background: js,
       title: 'Slice vs Splice',
       description:
         'There is often a confusion between slice and splice JavaScript array methods. It is a commonly asked question in Javascript interview for freshers.',
       link: 'https://dev.to/ip127001/slice-vs-splice-hmd',
     },
     {
-      background: array,
       title: 'Copying in JavaScript',
       description:
         'There is concept of shallow copy and deep copy in JavaScript. Through this article I tried to explain the concept and some userful resources.',
       link: 'https://dev.to/ip127001/copying-objects-in-javascript-440b',
     },
     {
-      background: objects,
       title: 'Convert an array to an object',
       description:
         'Generally, when we need to convert an array to object, we can use reduce method. I used an example to explain in this article.',
       link: 'https://dev.to/ip127001/convert-an-array-to-an-object-pne',
     },
     {
-      background: js,
       title: '5 ways to delete an element from array',
       description:
         'There is a situation when we need to delete elements from array based on conditions or given index of array. This article covers 5 days to do that and which on I prefer.',
@@ -37,12 +29,16 @@ function Blog() {
         'https://dev.to/ip127001/5-ways-to-delete-an-element-from-an-array-in-javascript-jm9',
     },
     {
-      background: shouldComponentUpdate,
       title: 'Performance optimization in react',
       description:
         'The most discussed performance technique is to avoid reconciliation and I have discussed throughly in this blog',
       link:
         'https://dev.to/ip127001/avoid-reconciliation-using-shouldcomponent-550m',
+    },
+    {
+      title: 'Truncate strings',
+      description: 'Truncate any string to align text in one line and show ellipsis at the end and show full text on hover',
+      link: 'https://dev.to/ip127001/truncate-string-with-ellipsis-26l5'    
     },
   ]
 
@@ -62,11 +58,6 @@ function Blog() {
           return (
             <div className="single-blog">
               <div className="layer-2"></div>
-              {/* <img
-                src={element.background}
-                alt="card"
-                className="blog-background"
-              /> */}
               <div className="blog-desc">
                 <span className="blog-desc__title">{element.title}</span>
                 <span className="blog-desc__description">
